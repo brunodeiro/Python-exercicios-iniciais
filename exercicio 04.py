d110e7762f3dd965a48fe4cb6fbe916e79bd0ec5
Bruno <bruno_deiro@live.com>
2023-01-16 14:24:29 -0300

    exercicio 04

---
+++ exercicio 04.py
@@ -0,0 +1,9 @@
+""" DESAFIO 03:
+- Informe um valor em reais e converta para dolar (cotação atual = 5,12).
+- Retorne o resultado."""
+
+n1 = float(input("Digite um valor em reais: "))
+
+dolar = n1 / 5.12
+
+print("Você possui {:.4} dolares".format(dolar))
